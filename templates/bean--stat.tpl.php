e77bb7527d0f2b15d41b06a04490ea0a23fbe19f
Carney, Sarah A <sacarney@iastate.edu>
2019-01-08 13:49:07 -0600

    LUGG-1144 Add conditional statements

--- templates/bean--stat.tpl.php
+++ templates/bean--stat.tpl.php
@@ -1,21 +1,29 @@
 <div class="stat-card">
   <?php print render($title_suffix) ?>
 
-  <div class="stat-card_icon">
-    <span class="fa <?php print $bean->field_stat_icon['und'][0]['value']; ?>"></span>
-  </div>
+  <?php if (isset($bean->field_stat_icon['und'][0]['value'])): ?>
+    <div class="stat-card_icon">
+      <span class="fa <?php print $bean->field_stat_icon['und'][0]['value']; ?>"></span>
+    </div>
+  <?php endif; ?>
 
   <div class="stat-card_content">
 
-    <div class="stat-card_title">
-      <?php print $title; ?>
-    </div>
+    <?php if ($bean->title !== ''): ?>
+      <div class="stat-card_title">
+        <?php print $title; ?>
+      </div>
+    <?php endif; ?>
 
-    <div class="stat-card_caption">
-      <?php print $bean->field_stat_caption['und'][0]['value'] ?>
-    </div>
+    <?php if (isset($bean->field_stat_caption['und'][0]['value'])): ?>
+      <div class="stat-card_caption">
+        <?php print $bean->field_stat_caption['und'][0]['value'] ?>
+      </div>
+    <?php endif; ?>
 
-    <a class="stat-card_button" href="<?php print $bean->field_stat_button['und'][0]['url']; ?>"><?php print $bean->field_stat_button['und'][0]['title']; ?></a>
+    <?php if (isset($bean->field_stat_button['und'][0]['url'])): ?>
+      <a class="stat-card_button" href="<?php print $bean->field_stat_button['und'][0]['url']; ?>"><?php print $bean->field_stat_button['und'][0]['title']; ?></a>
+    <?php endif; ?>
 
   </div>
 </div>
